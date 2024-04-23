d0afbd6f9039c8034815966e3688eb3654417fee
thaminultazbir <thaminultazbir1999@gmail.com>
2024-04-23 11:13:55 +0600

    Hero Section Complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,28 @@ module.exports = {
       lg: '960px',
       xl: '1200px',
     },
-    extend: {},
+    fontFamily:{
+      primary: 'DM Serif Display',
+      secondary: 'Jost'
+    },
+    backgroundImage:{
+      hero: 'url(/assets/hero/bg.jpg)',
+      grid: 'url(/assets/grid.png)',
+    },
+    extend: {
+      colors:{
+        primary: {
+          DEFAULT: '#292F36',
+          hover: '#343e41',
+        },
+        secondary: '#4D5053',
+        accent: {
+          DEFAULT: '#cda274',
+          secondary: '#f4f0ec',
+          hover:'#b88c5d'
+        },
+      },
+    },
   },
   plugins: [],
 }
