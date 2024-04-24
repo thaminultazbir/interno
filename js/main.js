6886388ba51077365e028c7ff92ed72e9ed67e0a
thaminultazbir <thaminultazbir1999@gmail.com>
2024-04-24 11:18:30 +0600

    Navbar Issued

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const navTriggerBtn = document.querySelector("#nav_trigger_btn");
+const navMenu = document.querySelector("#nav_menu");
+
+navTriggerBtn.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-is-open");
+});
